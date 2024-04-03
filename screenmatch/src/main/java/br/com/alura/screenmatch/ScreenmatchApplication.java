1ead60746f6554d522400db1a7d55566a0ee55f6
Lucas <lucaas_batista@hotmail.com>
2024-04-03 18:13:58 -0300

    Modelado os dados da aplicação e criado menu de iteração

--- screenmatch/src/main/java/br/com/alura/screenmatch/ScreenmatchApplication.java
+++ screenmatch/src/main/java/br/com/alura/screenmatch/ScreenmatchApplication.java
@@ -1,8 +1,6 @@
 package br.com.alura.screenmatch;
 
-import br.com.alura.screenmatch.model.DadosSerie;
-import br.com.alura.screenmatch.service.ConsumoApi;
-import br.com.alura.screenmatch.service.ConverteDados;
+import br.com.alura.screenmatch.principal.Principal;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -17,13 +15,9 @@ public class ScreenmatchApplication implements CommandLineRunner {
 	@Override
 	public void run(String... args) throws Exception {
 
-		var consumoApi = new ConsumoApi();
-		var json = consumoApi.obterDados("https://omdbapi.com/?t=prison+break&apikey=d5591da2");
-
-		ConverteDados conversor = new ConverteDados();
-		DadosSerie dados = conversor.obterDados(json, DadosSerie.class);
+		Principal principal = new Principal();
 
 		System.out.println("Primeiro projeto Spring sem Web!");
-		System.out.println(dados);
+		principal.exibeMenu();
 	}
 }
